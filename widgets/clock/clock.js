bf2030da31853bbacd4f176d76e637a28af94dc8
Vesa Frijling <vesa@fair2.media>
2018-05-02 00:08:07 -0400

    Updated clock widget to pie chart

--- widgets/clock/clock.js
+++ widgets/clock/clock.js
@@ -1,7 +1,23 @@
 window.addEventListener("load", function() {
     var css = '<link rel="stylesheet" type="text/css" href="widgets/clock/style.css">'
-    var html = '<p class="clockWidget" id="clock"></p>';
-    initializeWidget("clock", html, css, startClock);
+    var html = `<div class="pie">
+        <div class="clip1">
+            <div class="slice1"></div>
+        </div>
+        <div class="clip2">
+            <div class="slice2"></div>
+        </div>
+        <div class="status"></div>
+    </div>`;//'<p class="clockWidget" id="clock"></p>';
+    initializeWidget("clock", html, css, function() {
+        let currentdate = new Date();
+        let min = currentdate.getMinutes();
+        let hrs = currentdate.getHours();
+        if (hrs > 12) {
+            hrs -= 12;
+        }
+        progressBarUpdate(hrs + min/60, 12);
+    });
 });
 
 function startClock() {
@@ -21,4 +37,42 @@ function startClock() {
             clearInterval(clockInterval);
         }
     }, 1000);
+}
+
+function rotate(element, degree) {
+    // element.css({
+    //     '-webkit-transform': 'rotate(' + degree + 'deg)',
+    //         '-moz-transform': 'rotate(' + degree + 'deg)',
+    //         '-ms-transform': 'rotate(' + degree + 'deg)',
+    //         '-o-transform': 'rotate(' + degree + 'deg)',
+    //         'transform': 'rotate(' + degree + 'deg)',
+    //         'zoom': 1
+    // });
+    element.style.transform = "rotate(" + degree + "deg)";
+    element.style.zoom = 1;
+}
+
+function progressBarUpdate(x, outOf) {
+    var firstHalfAngle = 180;
+    var secondHalfAngle = 0;
+
+    // caluclate the angle
+    var drawAngle = x / outOf * 360;
+
+    // calculate the angle to be displayed if each half
+    if (drawAngle <= 180) {
+        firstHalfAngle = drawAngle;
+    } else {
+        secondHalfAngle = drawAngle - 180;
+    }
+
+    // set the transition
+    rotate(document.getElementsByClassName("slice1")[0], firstHalfAngle);
+    rotate(document.getElementsByClassName("slice2")[0], secondHalfAngle);
+    //rotate($(".slice1"), firstHalfAngle);
+    //rotate($(".slice2"), secondHalfAngle);
+
+    // set the values on the text
+    //$(".status").html(x + " of " + outOf);
+    document.getElementsByClassName("status")[0].innerHTML = x + " of " + outOf;
 }
