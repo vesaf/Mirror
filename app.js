0de323560e50fb002c7657c27c94841f6ee56ddc
Vesa Frijling <vesa@fair2.media>
2018-05-05 08:58:46 -0400

    Integrated server

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'public'));
-// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,14 +23,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log("ERROR:");
+  console.log(err.status);
+  console.log(err.message);
+  console.log("END ERROR");
   // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
-  res.send(404);
+  res.send(err.status || 500);
 });
 
 module.exports = app;
