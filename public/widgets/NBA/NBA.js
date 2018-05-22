14ac38773e7f9b29e6a4c6db3794f209393020ce
Vesa Frijling <vesa@fair2.media>
2018-05-22 15:06:58 +0200

    Got started on NBA widget with files and icon

---
+++ public/widgets/NBA/NBA.js
@@ -0,0 +1,7 @@
+var clockInterval
+window.addEventListener("load", function() {
+    var css = '<link rel="stylesheet" type="text/css" href="widgets/NBA/style.css">';
+    var html = ``;
+    initializeWidget("NBA", html, css);
+});
+
