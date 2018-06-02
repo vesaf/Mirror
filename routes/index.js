7a9d7bac65ef4467e2bf6c5b4a2fd698ecceab0d
Vesa Frijling <vesa@fair2.media>
2018-06-02 12:18:54 +0200

    Correct spotify request

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/spotify', function(req, res) {
-  https.get('https://accounts.spotify.com/en/authorize?client_id=c933d6e92605451aa4347afcf5e05c3c&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email&state=34fFs29kd09', (resp) => {
-    let data = '';
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-    resp.on('end', () => {
-      res.send(data);
-    });
-  }).on('error', (err) => {
-    console.log("Error: " + err.message);
-  });
-  // res.send('hello');
+  res.redirect('https://accounts.spotify.com/en/authorize?client_id=c933d6e92605451aa4347afcf5e05c3c&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email&state=34fFs29kd09');
 });
 
 module.exports = router;
