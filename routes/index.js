814fdf21709e34c48799e7b0c96b76921a35be4c
Vesa Frijling <vesa@fair2.media>
2018-06-24 13:58:28 +0200

    Changed port among other things

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,4 @@ router.get('/', function(req, res) {
   res.sendFile('./public/index.html');
 });
 
-router.get('/spotify', function(req, res) {
-  res.redirect('https://accounts.spotify.com/en/authorize?client_id=c933d6e92605451aa4347afcf5e05c3c&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email&state=34fFs29kd09');
-});
-
 module.exports = router;
