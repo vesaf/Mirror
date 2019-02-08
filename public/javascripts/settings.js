10fac23fbaf4cd8b93faad37f8c5749195b40755
Vesa Frijling <vesa@fair2.media>
2019-02-08 10:56:24 +0100

    Added working settings button

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -15,15 +15,41 @@ window.addEventListener("load", () => {
 });
 
 var open = false;
+// Open settings
 function openSettings() {
     if (!open) {
         open = true;
+        // Create opaque container
         var opaque = document.createElement("div");
         opaque.id = "opaqueLayer";
         var container = document.getElementById("container");
+        // Create side menu container
         var sideMenu = document.createElement("div");
         sideMenu.id = "settingsSideMenu";
+        // Create close button
+        var closeBtn = document.createElement("div");
+        closeBtn.id = "closeBtn"
+        closeBtn.addEventListener("click", closeSettings)
+        // Add to page
         opaque.appendChild(sideMenu);
+        opaque.appendChild(closeBtn);
         container.appendChild(opaque);
     }
+}
+
+// Close settings
+function closeSettings() {
+    // Identify relevant elements
+    var opaque = document.getElementById("opaqueLayer");
+    var container = document.getElementById("container");
+    var closeBtn = document.getElementById("closeBtn");
+    // Remove click listener
+    closeBtn.removeEventListener("click", closeSettings);
+    // Start closing animation
+    opaque.style.animationName = "downAnim";
+    // When closing animation (almost) done, remove elements
+    setTimeout(function () {
+        container.removeChild(opaque);
+        open = false;
+    }, 490);
 }
