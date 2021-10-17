d67fa39e0b73bce7d71aeda9e34434b9fb9a5cd7
Vesa Frijling <vesaf@msn.com>
2021-10-17 16:44:11 +0200

    Added HTML and CSS template for monitor timers

--- public/settings/monitorTiming.js
+++ public/settings/monitorTiming.js
@@ -1,10 +1,37 @@
 const monitorTiming = {
     title: "Monitor Timing",
     html: `
-    <div id='existingTimersContainer'></div>
-    <div>
-    <div class=footerContainer>
-        <button class=widgetSettingsBtn id=addMonitorTimingBtn>Add Timer</button>
+    <div id="existingTimersContainer">
+        <div class="timerContainer">
+            <div class="timeBtnContainer">
+                <div class="arrowUp decHourOnBtn"></div>
+                <div class="arrowUp unitHourOnBtn"></div>
+                <div class="arrowUp decMinuteOnBtn"></div>
+                <div class="arrowUp unitMinuteOnBtn"></div>
+                <div class="arrowUp decHourOffBtn"></div>
+                <div class="arrowUp unitHourOffBtn"></div>
+                <div class="arrowUp decMinuteOffBtn"></div>
+                <div class="arrowUp unitMinuteOffBtn"></div>
+            </div>
+            <div class="timeLabelContainer">
+                <p class="timeLabel">On 10:00</p>
+                <p class="timeLabel timeOffLabel">Off 17:00</p>
+                <i class="fa fa-trash deleteTimerBtn"></i>
+            </div>
+            <div class="timeBtnContainer">
+                <div class="arrowDown decHourOnBtn"></div>
+                <div class="arrowDown unitHourOnBtn"></div>
+                <div class="arrowDown decMinuteOnBtn"></div>
+                <div class="arrowDown unitMinuteOnBtn"></div>
+                <div class="arrowDown decHourOffBtn"></div>
+                <div class="arrowDown unitHourOffBtn"></div>
+                <div class="arrowDown decMinuteOffBtn"></div>
+                <div class="arrowDown unitMinuteOffBtn"></div>
+            </div>
+        </div>
+    </div>
+    <div class="footerContainer">
+        <button class="widgetSettingsBtn" id="addMonitorTimingBtn">Add Timer</button>
     </div>
     `,
     css: true,
